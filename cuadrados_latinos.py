6e341398e8c7d6f025345e4e2dbd31407c362ac6
Alejandro González Yáñez <alejandro.gy96@gmail.com>
2020-01-15 15:21:18 -0600

    Update cuadrados_latinos.py

--- cuadrados_latinos.py
+++ cuadrados_latinos.py
@@ -1,5 +1,5 @@
 class Graph:
-	def __init__(self,graph):
+	def __init__(self, graph):
 		self.graph = graph
 		self.ppl = len(graph)
 		self.jobs = len(graph[0])
@@ -13,7 +13,7 @@ class Graph:
 					return True
 		return False
 
-	def maxBPM(self, n):
+	def maxBPM(self):
 		matchR = [-1] * self.jobs
 		result = 0
 		for i in range(self.ppl):
@@ -22,33 +22,36 @@ class Graph:
 				result += 1
 		return result, matchR
 
+
+
 if __name__ == "__main__":
    size_square = int(input( ))
-   latin_square = []
-   for i in range(0, size_square + 1):
-      latin_square.append([0] * (size_square + 1))
 
-   for i in range(0, size_square + 1):
-      for j in range(0, size_square + 1):
-         latin_square[i][j] = int(input( ))
-
+   latin_square = [[0] * (size_square + 1) for i in range(0, size_square + 1)]
    for i in range(0, 2 * size_square):
-      r = int(input( ))
-      c = int(input( ))
-      e = int(input( ))
+      r, c, e = map(int, input( ).split( ))
+      latin_square[r][c] = e
+   print(latin_square)
 
+   # bpGraph = [
+   #    [0, 1, 1, 0, 0, 0],
+   #    [1, 0, 0, 1, 0, 0],
+   #    [0, 0, 1, 0, 0, 0],
+   #    [0, 0, 1, 1, 0, 0],
+   #    [0, 0, 0, 0, 0, 1],
+   #    [0, 0, 0, 0, 1, 1]
+   # ]
 
-   bpGraph = [
-      [0, 1, 1, 0, 0, 0],
-      [1, 0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0, 0],
-      [0, 0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 0, 1, 1]
-   ]
+   bpGraph = [[1] * (size_square + 1) for i in range(0, size_square + 1)]
+    for j in range(1, size_square + 1):
+      for i in range(1, size_square + 1):
+         if latin_square[i][j] != 0:
+            bpGraph[i][j] = 0
+
+   print(bpGraph)
 
    g = Graph(bpGraph)
-   res = g.maxBPM(6)
+   res = g.maxBPM( )
    print(res[0])
    print(res[1])
    
