6eb167d55483a26e6e6cf27372d40c7119e06dfa
AlejandroGY <alejandro.gy96@gmail.com>
2020-01-30 19:00:21 -0600

    Avance

--- CuadradosLatinos/cuadrados_latinos.py
+++ CuadradosLatinos/cuadrados_latinos.py
@@ -1,26 +1,26 @@
 class Graph:
-	def __init__(self, graph):
-		self.graph = graph
-		self.ppl = len(graph)
-		self.jobs = len(graph[0])
+   def __init__(self, graph):
+      self.graph = graph
+      self.ppl = len(graph)
+      self.jobs = len(graph[0])
 
-	def bpm(self, u, matchR, seen):
-		for v in range(self.jobs):
-			if self.graph[u][v] and seen[v] == False:
-				seen[v] = True
-				if matchR[v] == -1 or self.bpm(matchR[v], matchR, seen):
-					matchR[v] = u
-					return True
-		return False
+   def bpm(self, u, matchR, seen):
+      for v in range(self.jobs):
+         if self.graph[u][v] and seen[v] == False:
+            seen[v] = True
+            if matchR[v] == -1 or self.bpm(matchR[v], matchR, seen):
+               matchR[v] = u
+               return True
+      return False
 
-	def maxBPM(self):
-		matchR = [-1] * self.jobs
-		result = 0
-		for i in range(self.ppl):
-			seen = [False] * self.jobs
-			if self.bpm(i, matchR, seen):
-				result += 1
-		return result, matchR
+   def maxBPM(self):
+      matchR = [-1] * self.jobs
+      result = 0
+      for i in range(self.ppl):
+         seen = [False] * self.jobs
+         if self.bpm(i, matchR, seen):
+            result += 1
+      return result, matchR
 
 if __name__ == "__main__":
    size_square = int(input( ))
@@ -40,8 +40,10 @@ if __name__ == "__main__":
 
       graph = Graph(bpGraph)
       matching = graph.maxBPM( )
+      print(matching)
       for i in range(0, size_square):
          latin_square[t][i] = matching[1][i] + 1
 
+   print()
    for i in range(0, size_square):
       print(latin_square[i])
