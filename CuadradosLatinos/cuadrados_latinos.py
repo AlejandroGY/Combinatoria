0f8024e193cbf85fb10364e2ba82785679299916
AlejandroGY <alejandro.gy96@gmail.com>
2020-02-02 23:49:42 -0600

    Avance

--- CuadradosLatinos/cuadrados_latinos.py
+++ CuadradosLatinos/cuadrados_latinos.py
@@ -1,64 +1,72 @@
-class Graph:
-   def __init__(self, graph):
-      self.graph = graph
-      self.ppl = len(graph)
-      self.jobs = len(graph[0])
+class Grafo:
+   def __init__(self, grafo):
+      self.grafo = grafo
+      self.casillas = len(grafo)
+      self.numeros = len(grafo[0])
 
-   def bpm(self, u, matchR, seen):
-      for v in range(self.jobs):
-         if self.graph[u][v] and seen[v] == False:
-            seen[v] = True
-            if matchR[v] == -1 or self.bpm(matchR[v], matchR, seen):
-               matchR[v] = u
+   def bpm(self, u, acoplamiento_, visto):
+      for v in range(self.numeros):
+         if self.grafo[u][v] and visto[v] == False:
+            visto[v] = True
+            if acoplamiento_[v] == -1 or self.bpm(acoplamiento_[v], acoplamiento_, visto):
+               acoplamiento_[v] = u
                return True
       return False
 
    def maxBPM(self):
-      matchR = [-1] * self.jobs
-      result = 0
-      for i in range(self.ppl):
-         seen = [False] * self.jobs
-         if self.bpm(i, matchR, seen):
-            result += 1
-      return result, matchR
+      acoplamiento_ = [-1] * self.numeros
+      resultado = 0
+      for i in range(self.casillas):
+         visto = [False] * self.numeros
+         if self.bpm(i, acoplamiento_, visto):
+            resultado += 1
+      return resultado, acoplamiento_
+
+def resuelve_1(matriz, acumulado, tam):
+   for actual in acumulado:
+      adyacencia = [[1] * (tam) for i in range(0, tam)]
+      if actual[0] != 0:
+         for j in range(0, tam):
+            if matriz[actual[1]][j] != 0:
+               for i in range(0, tam):
+                  adyacencia[i][j] = 0
+               for i in range(0, tam):
+                  adyacencia[matriz[actual[1]][j] - 1][i] = 0
+      else:
+         for j in range(0, tam):
+            for i in range(0, tam):
+               if matriz[i][j] != 0:
+                  adyacencia[matriz[i][j] - 1][j] = 0
+
+      grafo = Grafo(adyacencia)
+      acoplamiento = grafo.maxBPM( )
+      
+      print()
+      print(adyacencia)
+      print(acoplamiento)
+      for i in range(0, tam):
+         if acoplamiento[1][i] != -1:
+            matriz[actual[1]][i] = acoplamiento[1][i] + 1
+   return matriz
 
 if __name__ == "__main__":
-   size_square = int(input( ))
-   number_elements = int(input( ))
-
-   accountant = [[0, i] for i in range(0, size_square)]
-   latin_square = [[0] * (size_square) for i in range(0, size_square)]
-   for i in range(0, number_elements):
+   tam, n = map(int, input( ).split( ))
+   acumulado = [[0, i] for i in range(0, tam)]
+   matriz = [[0] * (tam) for i in range(0, tam)]
+   for i in range(0, n):
       r, c, e = map(int, input( ).split( ))
-      latin_square[r - 1][c - 1] = e
-      accountant[r - 1][0] += 1
+      matriz[r - 1][c - 1] = e
+      acumulado[r - 1][0] += 1
 
-   accountant.sort(reverse = True)
-   print(accountant)
+   acumulado.sort(reverse = True)
+   print(acumulado)
    
    print("\nCuadrado Inicial:")
-   for i in range(0, size_square):
-      print(latin_square[i])
+   for i in range(0, tam):
+      print(matriz[i])
 
-   for t in accountant:
-      if t[0] != 0:
-         bpGraph = [[1] * (size_square) for i in range(0, size_square)]
-         for j in range(0, size_square):
-            if latin_square[t[1]][j] != 0:
-               for i in range(0, size_square):
-                  bpGraph[i][j] = 0
-               for i in range(0, size_square):
-                  bpGraph[latin_square[t[1]][j] - 1][i] = 0
-
-         graph = Graph(bpGraph)
-         matching = graph.maxBPM( )
-         
-         print(bpGraph)
-         print(matching)
-         for i in range(0, size_square):
-            if matching[1][i] != -1:
-               latin_square[t[1]][i] = matching[1][i] + 1
+   cuadrado_latino = resuelve_1(matriz, acumulado, tam)
 
    print("\nCuadrado Resuelto:")
-   for i in range(0, size_square):
-      print(latin_square[i])
+   for i in range(0, tam):
+      print(cuadrado_latino[i])
